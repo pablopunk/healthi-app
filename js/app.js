d4a23b725160ab13e87757e3f37b496d23338cc3
Pablo Varela <pablovarela182@gmail.com>
2016-10-12 17:04:24 -0400

    icon

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var menubar = require('menubar')
 var mb = menubar({
-    width: 200,
-    height: 80
+  width: 200,
+  height: 80,
+  icon: __dirname+"/../img/topbar.png"
 })
 
 mb.on('ready', function ready() {
