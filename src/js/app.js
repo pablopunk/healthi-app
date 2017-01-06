31a408291cb4f392c677c42ae87a19f99d97c077
Pablo Varela <pablovarela182@gmail.com>
2017-01-06 10:55:00 -0500

    🐛  Fix packaged app not showing window [fix #2]
    It was a bug in `menubar`, showing the dock icon and then hiding it after the window is open fixes the error

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,14 @@ const mb = menubar({
   width: 200,
   height: 90,
   icon: path.join(__dirname, '/../../img/topbarTemplate.png'),
-  dir: path.join(__dirname, '..')
+  dir: path.join(__dirname, '..'),
+  showDockIcon: true
 })
 
 mb.on('ready', () => {
   console.log('app is ready')
   mb.showWindow()
+  mb.app.dock.hide()
   globalShortcut.register('CommandOrControl+Alt+I', () => {
     mb.window.toggleDevTools()
   })
