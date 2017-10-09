bec537f7ef9175e36d9703bff908b382ce51bad2
Pablo Varela <pablovarela182@gmail.com>
2017-10-09 14:42:23 +0200

    Change hover effect on close button

--- static/js/components/content.js
+++ static/js/components/content.js
@@ -1,33 +1,33 @@
-'use strict'
+'use strict';
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
-})
+});
 
-var _react = require('react')
+var _react = require('react');
 
-var _react2 = _interopRequireDefault(_react)
+var _react2 = _interopRequireDefault(_react);
 
-var _reactFade = require('react-fade')
+var _reactFade = require('react-fade');
 
-var _reactFade2 = _interopRequireDefault(_reactFade)
+var _reactFade2 = _interopRequireDefault(_reactFade);
 
-var _status = require('./status')
+var _status = require('./status');
 
-var _status2 = _interopRequireDefault(_status)
+var _status2 = _interopRequireDefault(_status);
 
-var _buttons = require('./buttons')
+var _buttons = require('./buttons');
 
-var _buttons2 = _interopRequireDefault(_buttons)
+var _buttons2 = _interopRequireDefault(_buttons);
 
-function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class Content extends _react2.default.Component {
-  constructor (props) {
-    super(props)
-    this.state = { mouseEnter: false }
+  constructor(props) {
+    super(props);
+    this.state = { mouseEnter: false };
   }
-  render () {
+  render() {
     return _react2.default.createElement(
       'div',
       {
@@ -41,7 +41,7 @@ class Content extends _react2.default.Component {
         _react2.default.createElement(_buttons2.default, { app: this.props.app })
       ),
       _react2.default.createElement(_status2.default, null)
-    )
+    );
   }
 }
-exports.default = Content
+exports.default = Content;
--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,23 @@
-'use strict'
+'use strict';
 
-var _reactDom = require('react-dom')
+var _reactDom = require('react-dom');
 
-var _reactDom2 = _interopRequireDefault(_reactDom)
+var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _react = require('react')
+var _react = require('react');
 
-var _react2 = _interopRequireDefault(_react)
+var _react2 = _interopRequireDefault(_react);
 
-var _electron = require('electron')
+var _electron = require('electron');
 
-var _electron2 = _interopRequireDefault(_electron)
+var _electron2 = _interopRequireDefault(_electron);
 
-var _content = require('./../static/js/components/content')
+var _content = require('./../static/js/components/content');
 
-var _content2 = _interopRequireDefault(_content)
+var _content2 = _interopRequireDefault(_content);
 
-function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const app = _electron2.default.remote.app
+const app = _electron2.default.remote.app;
 
-_reactDom2.default.render(_react2.default.createElement(_content2.default, { app: app }), document.getElementById('content'))
+_reactDom2.default.render(_react2.default.createElement(_content2.default, { app: app }), document.getElementById('content'));
